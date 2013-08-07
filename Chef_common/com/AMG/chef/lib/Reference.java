4d63fef0bdb7d2a78b6eccd3b31f01d330a01366
BlackD0gz <Manni_D_M@hotmail.com>
2013-08-06 20:48:07 -0600

    Start of mod adding Main java slasses for forge and Reference data as well as mcmod info

---
+++ Chef_common/com/AMG/chef/lib/Reference.java
@@ -0,0 +1,7 @@
+package com.AMG.chef.lib;
+
+public class Reference {
+	public static final String MOD_ID = "Chef";
+	public static final String MOD_NAME = "Master Chef";
+	public static final String VERSION = "0.0.1";
+}
